67a9e29e6092e8392f8109477d8bf781012b389d
Carolyne Lira <carolyne.lira@meliuz.com.br>
2022-01-03 18:05:29 -0300

    :art: refactor: Modifica propriedades de ICardData

--- src/components/TransactionCard/interfaces.ts
+++ src/components/TransactionCard/interfaces.ts
@@ -4,15 +4,10 @@ export interface ITransactionCard {
 
 export interface ICardData {
     type: 'positive' | 'negative';
-    title: string;
-    amount: string;
-    category: ICategory;
-    date: string;
-}
-
-export interface ICategory {
     name: string;
-    icon: string;
+    amount: string;
+    category: string;
+    date: string;
 }
 
 export interface ITransactionType {
