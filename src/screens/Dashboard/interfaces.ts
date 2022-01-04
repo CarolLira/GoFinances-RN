db5e4e854901190531163d3ba19f8ef44a9447a6
Carolyne Lira <carolyne.lira@meliuz.com.br>
2022-01-03 22:18:13 -0300

    :sparkles: feat: Exibe valores totais de entradas e saídas

--- src/screens/Dashboard/interfaces.ts
+++ src/screens/Dashboard/interfaces.ts
@@ -3,3 +3,14 @@ import { ICardData } from "../../components/TransactionCard/interfaces";
 export interface IListData extends ICardData {
     id: string;
 }
+
+export interface HighLightData {
+    entries: HighLightProps,
+    expensives: HighLightProps,
+    total: HighLightProps
+
+}
+
+export interface HighLightProps {
+    amount: string;
+}
