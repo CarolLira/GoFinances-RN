7de413c1c1d5836825c2f2ee2e0b563defb27f1c
Carolyne Lira <carolyne.lira@meliuz.com.br>
2022-01-12 14:06:07 -0300

    :sparkles: Adiciona card de histórico por categoria

---
+++ src/screens/Resume/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { HistoryCard } from '../../components/HistoryCard';
+
+import {
+    Container,
+    Header,
+    Title,
+} from './styles';
+
+export function Resume() {
+    return (
+        <Container>
+            <Header>
+                <Title>Resumo por categoria</Title>
+            </Header>
+            <HistoryCard
+                title="Casa"
+                amount="R$ 1200"
+                color="purple"
+            />
+        </Container>
+    );
+}
