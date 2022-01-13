64654469d79717a710337b4b78ade76e20394a4c
Carolyne Lira <carolyne.lira@meliuz.com.br>
2022-01-13 14:38:04 -0300

    :clown_face: Muda tela renderizada no App.tsx temporariamente

--- App.tsx
+++ App.tsx
@@ -12,6 +12,8 @@ import theme from './src/global/styles/theme';
 import { NavigationContainer } from '@react-navigation/native';
 import { AppRoutes } from './src/routes/app.routes';
 
+import { SignIn } from './src/screens/SignIn';
+
 import {
   useFonts,
   Poppins_400Regular,
@@ -34,7 +36,7 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <NavigationContainer>
         <StatusBar barStyle="light-content"/>
-        <AppRoutes />
+        <SignIn />
       </NavigationContainer>
     </ThemeProvider>
   );
